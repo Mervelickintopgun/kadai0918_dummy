9880a0f6663b612a2d062d6a6cfb7cf047d2045f
Mervelickintopgun <johnson3@hotmail.co.jp>
2020-09-18 22:57:50 +0900

    Add files via upload

---
+++ js03_haifu/js03/js/config.js
@@ -0,0 +1,14 @@
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBjgLCXyqtIPg_OgDQOShnSKHhNcuIjyZo",
+    authDomain: "dev17-c0e9c.firebaseapp.com",
+    databaseURL: "https://dev17-c0e9c.firebaseio.com",
+    projectId: "dev17-c0e9c",
+    storageBucket: "dev17-c0e9c.appspot.com",
+    messagingSenderId: "756877408655",
+    appId: "1:756877408655:web:6b530675edff19691f33fa"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
